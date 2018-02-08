f0522a0860f9db07fdcc20208ec63a4308e97229
jlu <json.cy.lu@gmail.com>
2018-02-07 17:13:58 -0800

    Initial Commit

---
+++ NYC SchoolsTests/NYC_SchoolsTests.swift
@@ -0,0 +1,36 @@
+//
+//  NYC_SchoolsTests.swift
+//  NYC SchoolsTests
+//
+//  Created by Jason Lu on 2/7/18.
+//  Copyright © 2018 Jason Lu. All rights reserved.
+//
+
+import XCTest
+@testable import NYC_Schools
+
+class NYC_SchoolsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
