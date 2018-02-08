a65cfe6ab1b9c9841e7753367e8e670af2ae928d
jlu <json.cy.lu@gmail.com>
2018-02-07 22:27:53 -0800

    Initial commit

---
+++ 20180207-JL-NYCSchools/SchoolDetailViewController.swift
@@ -0,0 +1,71 @@
+//
+//  SchoolDetailViewController.swift
+//  NYC Schools
+//
+//  Created by Jason Lu on 2/7/18.
+//  Copyright © 2018 Jason Lu. All rights reserved.
+//
+
+import UIKit
+
+class SchoolDetailViewController: UIViewController {
+    @IBOutlet weak var mathLabel: UILabel!
+    @IBOutlet weak var readingLabel: UILabel!
+    @IBOutlet weak var writingLabel: UILabel!
+    @IBOutlet weak var numTakersLabel: UILabel!
+    @IBOutlet weak var schoolNameLabel: UILabel!
+    
+    var dbn: String?
+    var schoolName: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let dbn = dbn, let schoolName = schoolName {
+            schoolNameLabel.text = schoolName + "'s SAT Scores"
+            getSchoolDetail(dbn)
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    /// Grabs the school detail and asynchronously updates the UI with the parsed data. Because the data is an array of one object,
+    /// we access the 0 element. Given more time, I would have constructed a better model to parse the data as a single object so I don't have
+    /// to access the 0 element.
+    /// - Parameter name: name of the school to grab details from.
+    func getSchoolDetail(_ dbn: String) {
+        SchoolDetail.schoolByName(dbn, completionHandler: { [unowned self] (schoolDetail, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let schoolDetail = schoolDetail else {
+                print("error getting school detail")
+                return
+            }
+            print(schoolDetail)
+            guard schoolDetail.count > 0 else { return }
+            DispatchQueue.main.async {
+                self.mathLabel.text = schoolDetail[0].sat_math_avg_score
+                self.readingLabel.text = schoolDetail[0].sat_critical_reading_avg_score
+                self.writingLabel.text = schoolDetail[0].sat_writing_avg_score
+                self.numTakersLabel.text = schoolDetail[0].num_of_sat_test_takers
+            }
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ 20180207-JL-NYCSchools/SchoolsTableViewController.swift
@@ -0,0 +1,74 @@
+//
+//  SchoolsTableViewController.swift
+//  NYC Schools
+//
+//  Created by Jason Lu on 2/7/18.
+//  Copyright © 2018 Jason Lu. All rights reserved.
+//
+
+import UIKit
+
+class SchoolsTableViewController: UITableViewController {
+    let schoolCellIdentifier = "schoolIdentifier"
+    let schoolDetailSegue = "ShowDetailSegue"
+    var schoolsArray: [School] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getAllSchools()
+    }
+    
+    /// Function to get all an array of all the school names in the database. Then reloads the tableview upon completion.
+    func getAllSchools() {
+        School.allSchools { [weak self] (schools, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let schools = schools else {
+                print("error getting schools")
+                return
+            }
+            self?.schoolsArray = schools
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return schoolsArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: schoolCellIdentifier, for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = schoolsArray[indexPath.row].school_name
+
+        return cell
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == schoolDetailSegue, let destination = segue.destination as? SchoolDetailViewController, let index = tableView.indexPathForSelectedRow?.row {
+            destination.schoolName = schoolsArray[index].school_name
+            destination.dbn = schoolsArray[index].dbn
+            print(schoolsArray[index].school_name)
+        }
+    }
+    
+
+}
